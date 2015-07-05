0d0946fbb0063c97a50305db3ceb3f59cf55113a
jyates999 <jyates999@gmail.com>
2015-07-05 14:05:52 -0400

    Nothing in this project yet

---
+++ AlphaV2/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="AlphaV2.Global" Language="C#" %>
---
+++ AlphaV2/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(AlphaV2.Startup))]
+namespace AlphaV2
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
